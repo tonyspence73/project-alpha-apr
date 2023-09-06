2ac29fb08271c211e176570b972803f794295760
Anthony Spence <anthonyspence08091973@gmail.com>
2023-09-06 08:23:35 -0700

    Feature 6 complete

--- projects/views.py
+++ projects/views.py
@@ -5,9 +5,15 @@ from .models import Project
 # Create your views here.
 
 
-def list_projects(request):
-    projects = Project.objects.all()
+# def list_projects(request):
+#     projects = Project.objects.all()
 
-    return render(
-        request, "projects/list_projects.html", {"projects": projects}
-    )
+
+#     return render(
+#         request, "projects/list_projects.html", {"projects": projects}
+#     )
+def list_projects(request):
+    projects = Project.objects.filter(owner=request.user)
+    context = {"projects": projects}
+
+    return render(request, "projects/list_projects.html", context)
--- tracker/urls.py
+++ tracker/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_to_home(request):
+    return redirect("/projects/")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls", namespace="projects")),
+    path("", redirect_to_home, name="home"),
 ]
